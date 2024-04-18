5932824a7a3b718fc8aa8b72ca40b704808a7b19
yzhang486 <78031365+yzhang486@users.noreply.github.com>
2024-04-18 00:31:42 -0400

    Add files via upload

--- yzhan486_problem_4_ex_1.py
+++ yzhan486_problem_4_ex_1.py
@@ -81,3 +81,7 @@ tif_path = "/content/drive/MyDrive/Neural_Signals_and_Computation_yzhan486/TEST_
 frames = read_tif_frames(tif_path)
 
 time_traces = extract_time_traces(frames, roi_masks)
+
+print(time_traces)
+plt.figure(figsize=(10,5))
+plt.plot(time_traces)
